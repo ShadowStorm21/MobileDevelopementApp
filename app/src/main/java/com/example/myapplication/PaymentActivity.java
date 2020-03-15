95160a288a461c6884e742733feed5f1415f900a
Muhannad <55150185+MMSR-X9@users.noreply.github.com>
2020-03-15 17:19:00 +0400

    new changes

--- app/src/main/java/com/example/myapplication/PaymentActivity.java
+++ app/src/main/java/com/example/myapplication/PaymentActivity.java
@@ -8,119 +8,133 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Calendar;
+
 public class PaymentActivity extends AppCompatActivity {
-    private Button pay;
-    private String user_id,username, product_id;
-    private double price;
-    private EditText cardHolderName,cardNumber,cardCvv;
-    private FirebaseDatabase database = FirebaseDatabase.getInstance();
-    private DatabaseReference myRef = database.getReference("Orders");
-    private TextView txtPrice;
+    private Button pay,clear;
+    private EditText cardNo,cardCVV;
+    private Spinner cardYear, cardMonth;
+    private RadioGroup paymentOptions;
+    private TextView price,errMsg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
-        pay = findViewById(R.id.pay);
-        setTitle("Payment");
-        Intent intent = getIntent();
-        txtPrice = findViewById(R.id.textView4);
+        pay = findViewById(R.id.btnPay);
+        clear = findViewById(R.id.btnClear);
+        price = findViewById(R.id.tvTotalPrice);
+        errMsg = findViewById(R.id.errMsg);
+        cardNo = findViewById(R.id.etCardNo);
+        cardMonth = findViewById(R.id.spCardMonth);
+        cardYear = findViewById(R.id.spCardYear);
+        cardCVV = findViewById(R.id.etCVV);
+        paymentOptions = findViewById(R.id.rgPaymentOptions);
 
-        if(intent.hasExtra("username"))
-        {
-            user_id = intent.getStringExtra("id");
-            username = intent.getStringExtra("username");
-            product_id = intent.getStringExtra("pid");
-            price = intent.getDoubleExtra("total",0);
-            txtPrice.setText("Total Price : "+ price + " $");
-
-        }
-        else
-        {
-            Intent intent1 = new Intent(PaymentActivity.this,LoginActivity.class);
-            startActivity(intent1);
-            finish();
-        }
-
+        final Double cartTotalPrice = getIntent().getDoubleExtra("total",0);
+        price.setText(  cartTotalPrice.toString() + " $");
+        price.setTextColor(Color.RED);
+        errMsg.setTextColor(Color.RED);
 
         pay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if(checkEditTextFields())
-                {
-                    insertRecordDatabase();
-                }
+                if( !isDataValid())
+                    return;
+                errMsg.setText("");
 
+                Intent intent = new Intent(PaymentActivity.this, MainActivity.class);
+                PendingIntent contentIntent = PendingIntent.getActivity(PaymentActivity.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                String CHANNEL_ID="LOLW";
+                NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID,"XD", NotificationManager.IMPORTANCE_LOW);
+                Notification notification = new NotificationCompat.Builder(PaymentActivity.this,CHANNEL_ID)
+                        .setTicker(getString(R.string.app_name))
+                        .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                        .setContentTitle(getString(R.string.app_name)).setContentText(getString(R.string.content))
+                        .setContentIntent(contentIntent)
+                        .setAutoCancel(true)
+                        .build();
+                NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+                notificationManager.createNotificationChannel(notificationChannel);
+                notificationManager.notify(10, notification);
+
+                insertRecordDatabase();
 
             }
         });
-    }
 
-
-    private void insertRecordDatabase()
-    {
-        // after paying add the record to database in a new parent
-        String order_id = myRef.push().getKey();
-        Orders order = new Orders(order_id,user_id,username,price);
-        myRef.child(order_id).setValue(order).addOnSuccessListener(new OnSuccessListener<Void>() {
+        paymentOptions.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onSuccess(Void aVoid) {
-
-               sendNotification();
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (paymentOptions.getCheckedRadioButtonId()){
+                    case R.id.rbCash: // disable the visa edit texts
+                        cardCVV.setEnabled(false);
+                        cardMonth.setEnabled(false);
+                        cardYear.setEnabled(false);
+                        cardNo.setEnabled(false);
+                        clear.setEnabled(false);
+                        break;
+                    case R.id.rbVisa: // enable the visa edit texts
+                        cardCVV.setEnabled(true);
+                        cardMonth.setEnabled(true);
+                        cardYear.setEnabled(true);
+                        cardNo.setEnabled(true);
+                        clear.setEnabled(true);
+                        break;
+                }
             }
         });
     }
 
-    private boolean checkEditTextFields()
-    {
-        cardHolderName = findViewById(R.id.editTextCardHolder);
-        cardNumber = findViewById(R.id.editTextCardNumber);
-        cardCvv = findViewById(R.id.editTextCVV);
+    private boolean isDataValid() {
+        if( paymentOptions.getCheckedRadioButtonId() == R.id.rbCash ) {
+            return true;
+        }
+        // check card data
 
-        if(cardHolderName.getText().toString().isEmpty() || cardNumber.getText().toString().isEmpty() || cardCvv.getText().toString().isEmpty())
-        {
-            Toast.makeText(this, "All fields are required", Toast.LENGTH_SHORT).show();
+        String cardNumber = cardNo.getText().toString();
+        String CVV = cardCVV.getText().toString();
+        Integer month = Integer.valueOf(cardMonth.getSelectedItem().toString()) - 1;
+        Integer year = Integer.valueOf(cardYear.getSelectedItem().toString());
+
+        Calendar calendar = Calendar.getInstance();
+        Integer curMonth = calendar.get(calendar.MONTH);
+        Integer curYear = calendar.get(calendar.YEAR);
+
+        if(cardNumber.length() != 16) {
+            errMsg.setText("Card Number must be 16 Number");
             return false;
         }
-        if(cardNumber.getText().toString().length() > 16)
-        {
-            Toast.makeText(this, "Card Number Must not exceed 16 Digits ", Toast.LENGTH_SHORT).show();
+        if(CVV.length() != 3){
+            errMsg.setText("CVV must be 3 Numbers");
             return false;
         }
-        if(cardCvv.getText().toString().length() > 3)
-        {
-            Toast.makeText(this, "CVV must not exceed 3 Digits ", Toast.LENGTH_SHORT).show();
+        if(year < curYear || month < curMonth){
+            errMsg.setText("Your Card is expired!");
             return false;
         }
+
         return true;
     }
 
-    private void sendNotification()
-    {
-        Intent intent = new Intent(PaymentActivity.this, MainActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(PaymentActivity.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        String CHANNEL_ID="LOLW";
-        NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID,"XD", NotificationManager.IMPORTANCE_LOW);
-        Notification notification = new NotificationCompat.Builder(PaymentActivity.this,CHANNEL_ID)
-                .setTicker(getString(R.string.app_name))
-                .setSmallIcon(android.R.drawable.sym_def_app_icon)
-                .setContentTitle(getString(R.string.app_name)).setContentText(getString(R.string.content))
-                .setContentIntent(contentIntent)
-                .setAutoCancel(true)
-                .build();
-        NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.createNotificationChannel(notificationChannel);
-        notificationManager.notify(10, notification);
+
+    private void insertRecordDatabase() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        String uid,username;
     }
 }
